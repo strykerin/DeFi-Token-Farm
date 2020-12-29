5f1ef9ca14ab52c8ec995690764c423b58b8577f
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-12-29 11:46:28 -0300

    deploy farm token

--- migrations/2_deploy_MyTokenAndFarmToken.js
+++ migrations/2_deploy_MyTokenAndFarmToken.js
@@ -1,7 +1,12 @@
 const MyToken = artifacts.require('MyToken')
+const FarmToken = artifacts.require('FarmToken')
 
 module.exports = async function(deployer, network, accounts) {
     // Deploy MyToken
     await deployer.deploy(MyToken)
     const myToken = await MyToken.deployed()
+
+    // Deploy Farm Token
+    await deployer.deploy(FarmToken, myToken.address)
+    const farmToken = await FarmToken.deployed()
 }
