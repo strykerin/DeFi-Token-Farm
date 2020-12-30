d64435a41ebf7efa1547d5c61b1759ed26f982f7
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-12-29 21:17:17 -0300

    improve console log

--- scripts/transferMyTokenToFarmToken.js
+++ scripts/transferMyTokenToFarmToken.js
@@ -22,16 +22,30 @@ module.exports = async function(callback) {
 
 
     // Verify accounts[0] and farmToken balance of MyToken before and after the transfer
-    balanceBeforeAccounts0 = await myToken.balanceOf(accounts[0]);
-    balanceBeforeFarmToken = await myToken.balanceOf(farmToken.address);
-    console.log('Balance MyToken Before MyToken accounts[0] ' + web3.utils.fromWei(balanceBeforeAccounts0.toString()))
-    console.log('Balance MyToken Before MyToken TokenFarm ' + web3.utils.fromWei(balanceBeforeFarmToken.toString()))
+    balanceMyTokenBeforeAccounts0 = await myToken.balanceOf(accounts[0]);
+    balanceMyTokenBeforeFarmToken = await myToken.balanceOf(farmToken.address);
+    console.log('***My Token***')
+    console.log('Balance MyToken Before accounts[0] ' + web3.utils.fromWei(balanceMyTokenBeforeAccounts0.toString()))
+    console.log('Balance MyToken Before TokenFarm ' + web3.utils.fromWei(balanceMyTokenBeforeFarmToken.toString()))
+
+    console.log('***Farm Token***')
+    balanceFarmTokenBeforeAccounts0 = await farmToken.balanceOf(accounts[0]);
+    balanceFarmTokenBeforeFarmToken = await farmToken.balanceOf(farmToken.address);
+    console.log('Balance FarmToken Before accounts[0] ' + web3.utils.fromWei(balanceFarmTokenBeforeAccounts0.toString()))
+    console.log('Balance FarmToken Before TokenFarm ' + web3.utils.fromWei(balanceFarmTokenBeforeFarmToken.toString()))
     // Call Deposit function from FarmToken
     await farmToken.deposit(1000);
-    balanceAfterAccounts0 = await myToken.balanceOf(accounts[0]);
-    balanceAfterFarmToken = await myToken.balanceOf(farmToken.address);
-    console.log('Balance MyToken After MyToken accounts[0] ' + web3.utils.fromWei(balanceAfterAccounts0.toString()))
-    console.log('Balance MyToken After MyToken TokenFarm ' + web3.utils.fromWei(balanceAfterFarmToken.toString()))
+    console.log('***My Token***')
+    balanceMyTokenAfterAccounts0 = await myToken.balanceOf(accounts[0]);
+    balanceMyTokenAfterFarmToken = await myToken.balanceOf(farmToken.address);
+    console.log('Balance MyToken After accounts[0] ' + web3.utils.fromWei(balanceMyTokenAfterAccounts0.toString()))
+    console.log('Balance MyToken After TokenFarm ' + web3.utils.fromWei(balanceMyTokenAfterFarmToken.toString()))
+
+    console.log('***Farm Token***')
+    balanceFarmTokenAfterAccounts0 = await farmToken.balanceOf(accounts[0]);
+    balanceFarmTokenAfterFarmToken = await farmToken.balanceOf(farmToken.address);
+    console.log('Balance FarmToken After accounts[0] ' + web3.utils.fromWei(balanceFarmTokenAfterAccounts0.toString()))
+    console.log('Balance FarmToken After TokenFarm ' + web3.utils.fromWei(balanceFarmTokenAfterFarmToken.toString()))
 
     // End function
     callback();
