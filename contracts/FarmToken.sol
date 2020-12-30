21fe377d89462327e8644bb77f1d0ef5f0f04492
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-12-30 19:32:13 -0300

    remove unnecessary import statements

--- contracts/FarmToken.sol
+++ contracts/FarmToken.sol
@@ -1,14 +1,11 @@
 pragma solidity ^0.6.2;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract FarmToken is ERC20 {
-    using SafeERC20 for IERC20;
     using Address for address;
     using SafeMath for uint256;
 
